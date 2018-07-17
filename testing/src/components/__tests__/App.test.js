50bea2685e4c85b1438ca4fd43a612b3e613d607
JaimeRC <jaime.rc@icloud.com>
2018-07-17 18:41:07 +0200

    Change test App

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
+import { shallow } from 'enzyme'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
-it('shows a comment box', () =>{
+let wrapped
 
-    const div = ReactDOM.create('div') //Creamos un div para renderizar nuestro componente para comenzar las pruebas
+beforeEach(() => {
+    wrapped = shallow(<App />)
+})
 
-    ReactDOM.render(<App />,div)
+it('shows a comment box', () => {
 
+    /*
+    const div = ReactDOM.createElement('div') //Creamos un div para renderizar nuestro componente para comenzar las pruebas
 
-    ReactDOM.unmountComponentAtNode(div) //Eliminamos el div anteriormente creado para finalizar los test
+    ReactDOM.render(<App />, div)
+
+    expect(div.innerHTML).toContain('Comment Box')
+
+    ReactDOM.unmountComponentAtNode(div) //Eliminamos el div anteriormente creado para finalizar los test*/
+
+    expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1)
 })
