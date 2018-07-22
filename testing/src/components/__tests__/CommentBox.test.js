abe2a694e5caebb51f63dcd2d8ac617cd9842d5c
JaimeRC <jaime.rc@icloud.com>
2018-07-22 13:41:35 +0200

    Test CommentList component

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import CommentBox from 'components/CommentBox'
 import { mount } from 'enzyme'
+import Root from 'Root'
 
 let wrapped
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />)
+    wrapped = mount(
+        <Root>
+            <CommentBox />
+        </Root>)
 })
 
 afterEach(() => {
@@ -15,7 +19,7 @@ afterEach(() => {
 it("has a text area and button", () => {
 
     expect(wrapped.find('textarea').length).toEqual(1)
-    expect(wrapped.find('button').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(2)
 })
 
 describe("the text area", () => {
