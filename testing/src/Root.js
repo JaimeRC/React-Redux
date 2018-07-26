cf9e70b9061b507ce8e514d6a33d6e3aa7da4f30
JaimeRC <jaime.rc@icloud.com>
2018-07-26 23:37:14 +0200

    add new middleware for Promise without using redux-promise

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise  from 'redux-promise'
+//import reduxPromise  from 'redux-promise' -> Middleware for Promise the Redux
+import async from 'middleware/async' // -> Middleware Async create for us
 import reducers from 'reducers'
 
 export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise))
+    const store = createStore(
+        reducers, 
+        initialState,
+        //applyMiddleware(reduxPromise)
+        applyMiddleware(async)
+    )
 
     return (
         <Provider store={store}>
