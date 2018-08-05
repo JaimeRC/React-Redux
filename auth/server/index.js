3180d80499ef6759b37152de0f1fcf24b1da3051
JaimeRC <jaime.rc@icloud.com>
2018-08-05 18:57:51 +0200

    add cors

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,18 +4,20 @@ const express = require('express'),
     morgan = require('morgan'),
     app = express(),
     router = require('./router'),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    cors = require('cors')
 
 //DB setup
-mongoose.connect('mongodb://localhost:27017/auth',{ useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true })
 
 //App setup
+app.use(cors)
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
 router(app)
 
 //Server Setup
-const port = process.env.PORT || 3090 
-    server = http.createServer(app)
+const port = process.env.PORT || 3090
+server = http.createServer(app)
 server.listen(port)
 console.log('Server listening on: ', port)
